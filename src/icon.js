83602de833dad9d2f43f5b0a25782531fdc8ba82
Sean Purcell <s3purcell@gmail.com>
2021-01-17 11:22:08 -0800

    add icons

--- src/icon.js
+++ src/icon.js
@@ -1,5 +1,24 @@
-import { mdiAccount } from '@mdi/js';
+import { mdiWeatherPartlyCloudy, mdiWeatherCloudy, mdiAlertDecagram } from '@mdi/js';
+import m from 'mithril';
 
 export class Icon {
-    
+    view(vnode) {
+        return <div class="m-icon">
+            <svg viewBox="0 0 24 24" >
+                <path d={this.getIcon(vnode.attrs.icon)} />
+            </svg >
+        </div >
+    }
+
+    getIcon(name) {
+        switch (name) {
+            case 'weather-partly-cloudy':
+                return mdiWeatherPartlyCloudy;
+            case 'weather-cloudy':
+                return mdiWeatherCloudy;
+            default:
+                return mdiAlertDecagram
+        }
+
+    }
 }
