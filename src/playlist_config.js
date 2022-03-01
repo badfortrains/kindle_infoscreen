1f0ae7ddd905656ab6236bce1f434c1ded6fad24
Sean Purcell <s3purcell@gmail.com>
2022-03-01 11:53:41 -0800

    update playlist format

--- src/playlist_config.js
+++ src/playlist_config.js
@@ -1,31 +1,31 @@
 export const playlistConfig = [
     {
-        id: 'https://open.spotify.com/playlist/02NWwQw8Mqg51bfHijRaFr?si=d1vCICk-TaC0kkY4GAdXEA',
+        id: 'spotify:playlist:02NWwQw8Mqg51bfHijRaFr?si=d1vCICk-TaC0kkY4GAdXEA',
         displayName: 'Cowboy music',
         icon: 'dist/playlist_icons/svg/004-cowboy.svg',
     },
     {
-        id: 'https://open.spotify.com/playlist/3ZRM9VptVBRhSYwjriB2uo?si=IEqdY24nT5aFjUG3-TK0Pg',
+        id: 'spotify:playlist:3ZRM9VptVBRhSYwjriB2uo?si=IEqdY24nT5aFjUG3-TK0Pg',
         displayName: 'Alté music',
         icon: 'dist/playlist_icons/svg/005-nigeria.svg',
     },
     {
-        id: 'https://open.spotify.com/playlist/2bgI0QvdIbGduRgHFFvfT8?si=7BpknF7RS2Ov-jJqni3CHg',
+        id: 'spotify:playlist:2bgI0QvdIbGduRgHFFvfT8?si=7BpknF7RS2Ov-jJqni3CHg',
         displayName: 'Types of birds',
         icon: 'dist/playlist_icons/svg/003-eagle-1.svg',
     },
     {
-        id:'https://open.spotify.com/playlist/37i9dQZF1EMgS3xGrzVehB?si=U1TLzziOQsGSXxytJYVNwQ',
+        id:'spotify:playlist:37i9dQZF1EMgS3xGrzVehB?si=U1TLzziOQsGSXxytJYVNwQ',
         displayName: 'Top 2020 songs',
         icon: 'dist/playlist_icons/svg/006-like.svg',
     },
     {
-        id: 'https://open.spotify.com/playlist/7q503YgioHAbo1iOIa67M8?si=n2c4KhfdSWOJvNwipu9GAA',
+        id: 'spotify:playlist:7q503YgioHAbo1iOIa67M8?si=n2c4KhfdSWOJvNwipu9GAA',
         displayName: 'Pitchfork best new music',
         icon: 'dist/playlist_icons/svg/pitchfork.svg',
     },
     {
-        id: 'https://open.spotify.com/playlist/37i9dQZEVXcQtGfzxdXoFk?si=nBpHWO9JRDOUxQ51GoRpcw',
+        id: 'spotify:playlist:37i9dQZEVXcQtGfzxdXoFk?si=nBpHWO9JRDOUxQ51GoRpcw',
         displayName: 'Discover weekly',
         icon: 'dist/playlist_icons/svg/007-ship.svg',
     },
